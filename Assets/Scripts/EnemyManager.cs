1f5728e70cc3e579574c8ba74772cb4ab79fc19a
73N37 <x0d4r4p@proton.me>
2026-03-13 13:20:47 +0100

    Refactor EnemyManager and add new enemy prefab
    - Updated EnemyManager to include separate settings for fast, basic, and tank enemies.
    - Implemented dynamic enemy spawning logic that increases enemy count based on previous wave outcomes.
    - Added a new enemy prefab for the normal enemy type, including its components and settings.
    - Updated TagManager to include "Enemy" tag for better organization.

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -8,15 +8,24 @@ public class EnemyManager : MonoBehaviour
     public Transform[] checkpoints;
     public Transform spawnPoint;
 
-    [SerializeField] private GameObject enemy;
+    [Header("Fast Enemy settings")]
     [SerializeField] private GameObject fastEnemy;
-    [SerializeField] private GameObject tankEnemy;
-    [SerializeField] private int totalWaves = 3;
-    [SerializeField] private int enemyCount = 6;
     [SerializeField] private int fastEnemyCount = 2;
+
+    [Header("Basic Enemy settings")]
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private int enemyCount = 2;
+
+    [Header("Tank Enemy settings")]
+    [SerializeField] private GameObject tankEnemy;
     [SerializeField] private int tankEnemyCount = 1;
+
+    [Header("Wave settings")]
+    [SerializeField] private int totalWaves = 3;
     [SerializeField] private float spawnDelay = 0.5f;
     [SerializeField] private float timeBetweenWaves = 2f;
+
+    
 
     void Awake()
     {
@@ -26,6 +35,20 @@ public class EnemyManager : MonoBehaviour
     void Start()
     {
         StartCoroutine(SpawnWaves());
+    }
+
+private void Update()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if  
+        (
+            enemies.Length == 0
+        )
+        {
+            enemyCount += Mathf.RoundToInt(enemyCount * spawnDelay);
+            SpawnWaves();
+        }
     }
 
     private IEnumerator SpawnWaves()
@@ -50,18 +73,15 @@ public class EnemyManager : MonoBehaviour
 
         List<GameObject> waveSet = new List<GameObject>();
 
-        for (int i = 0; i < currentEnemyCount; i++)
-        {
+        for (int i = 0; i < currentEnemyCount; i++) {
             waveSet.Add(enemy);
         }
 
-        for (int i = 0; i < currentFastEnemyCount; i++)
-        {
+        for (int i = 0; i < currentFastEnemyCount; i++) {
             waveSet.Add(fastEnemy);
         }
 
-        for (int i = 0; i < currentTankEnemyCount; i++)
-        {
+        for (int i = 0; i < currentTankEnemyCount; i++) {
             waveSet.Add(tankEnemy);
         }
 
